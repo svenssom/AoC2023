e335059f86f54f3fe6a86abb586f58cf19581a15
Tove Svensson <svensson.tove@outlook.com>
2023-12-01 06:38:43 +0100

    Day 1 done, not exactly elegant...

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,28 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map {
+            it.find { it.isDigit() }.toString() + it.findLast { it.isDigit() }
+        }.sumOf { it.toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return part1(input.map {
+            it.replace("one","one1one")
+                .replace("two","two2two")
+                .replace("three","three3three")
+                .replace("four","four4four")
+                .replace("five","five5five")
+                .replace("six","six6six")
+                .replace("seven","seven7seven")
+                .replace("eight","eight8eight")
+                .replace("nine","nine9nine")
+        })
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("testinput")
-    check(part1(testInput) == 1)
+    println(part2(testInput))
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
